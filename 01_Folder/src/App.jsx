b244201b5ce804b357d0e590bdb119516d54ad55
Dilliram <bradjohn235@gmail.com>
2026-03-15 13:03:37 +0545

    Start of React

---
+++ 01_Folder/src/App.jsx
@@ -0,0 +1,20 @@
+// function App(){
+//   return <h1>Hello World</h1>;
+// }
+
+// export default App
+
+
+const App = () => {
+  return (
+    <> 
+    <div id="dad">
+      <h1>Hello</h1>
+      <h3>whatsupp</h3>
+    </div>
+     <div id="buwa"></div>
+    </>
+  )
+}
+
+export default App
